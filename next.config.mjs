d07d8a057d07181e89fb73aaa3ebd48555bdca67
Pal Bijewar <pal.bijewar@gmail.com>
2024-02-28 15:06:20 +0530

    Deployment relates fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['cdn1.iconfinder.com', "img.freepik.com", "1000logos.net", "www.edigitalagency.com.au", "encrypted-tbn0.gstatic.com"], 
+      domains: ['cdn1.iconfinder.com', "img.freepik.com", "1000logos.net", "www.edigitalagency.com.au", "encrypted-tbn0.gstatic.com", "miro.medium.com", "upload.wikimedia.org"], 
     },
   };
   
--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useRef } from "react";
-import { useScroll, useTransform, motion } from "framer-motion";
+import Image from "next/image";
+import React, { useRef } from 'react';
+import { useScroll, useTransform, motion } from 'framer-motion';
 
 export const ContainerScroll = ({
   users,
@@ -25,9 +26,9 @@ export const ContainerScroll = ({
       setIsMobile(window.innerWidth <= 768);
     };
     checkMobile();
-    window.addEventListener("resize", checkMobile);
+    window.addEventListener('resize', checkMobile);
     return () => {
-      window.removeEventListener("resize", checkMobile);
+      window.removeEventListener('resize', checkMobile);
     };
   }, []);
 
@@ -47,7 +48,7 @@ export const ContainerScroll = ({
       <div
         className="py-10 md:py-40 w-full relative"
         style={{
-          perspective: "1000px",
+          perspective: '1000px',
         }}
       >
         <Header translate={translate} titleComponent={titleComponent} />
@@ -94,10 +95,10 @@ export const Card = ({
   return (
     <motion.div
       style={{
-        rotateX: rotate, // rotate in X-axis
+        rotateX: rotate,
         scale,
         boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+          '0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003',
       }}
       className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
@@ -109,20 +110,22 @@ export const Card = ({
             style={{ translateY: translate }}
             whileHover={{
               boxShadow:
-                "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
+                '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
             }}
           >
             <div className="absolute top-2 right-2 rounded-full text-xs font-bold bg-white px-2 py-1">
               {user.badge}
             </div>
-            <img
+            <Image
               src={user.image}
-              className="rounded-tr-md rounded-tl-md text-sm "
               alt="thumbnail"
+              width={300}
+              height={300}
+              className="rounded-tr-md rounded-tl-md text-sm "
             />
             <div className="p-4">
-              <h1 className="font-semibold text-sm ">{user.name}</h1>
-              <h2 className=" text-gray-500 text-xs ">{user.designation}</h2>
+              <h1 className="font-semibold text-sm">{user.name}</h1>
+              <h2 className=" text-gray-500 text-xs">{user.designation}</h2>
             </div>
           </motion.div>
         ))}
